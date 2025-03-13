00fd9dd3bd08dda800bb252fcc8687970845dc27
marta <msanchezl@iesrafaelalberti.es>
2025-03-13 19:44:26 +0100

    script build render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(UrlGenerator $url)
+{
+    if (env('APP_ENV') === 'production') {
+        $url->forceScheme('https');
     }
+}
+
 }

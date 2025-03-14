d3c6fec330a4972c93c822aac3dc5088cfa665cf
marta <msanchezl@iesrafaelalberti.es>
2025-03-14 13:09:12 +0100

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,19 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-<<<<<<< HEAD
-    public function boot(UrlGenerator $url)
-{
-    if (env('APP_ENV') === 'production') {
-        $url->forceScheme('https');
-    }
-=======
     public function boot()
 {
     //
 }
 
->>>>>>> 5871ded (.)
 }
 
 }
